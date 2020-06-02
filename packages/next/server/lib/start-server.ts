bb536862390f01994ddcd09f8351201c4471728c
finleygn <finleygarton12@gmail.com>
2020-06-02 21:17:14 +0100

    added get certs and create http2 server method

--- packages/next/server/lib/start-server.ts
+++ packages/next/server/lib/start-server.ts
@@ -1,6 +1,13 @@
-import http from 'http'
+import http, { Server } from 'http'
+import http2, { Http2Server } from 'http2'
 import next from '../next'
+import getCerts from './get-certs'
 
+/**
+ * Build next handler and connect http server
+ *
+ * @throws {Error} Will throw when unable to create server because of port in use
+ */
 export default async function start(
   serverOptions: any,
   port?: number,
@@ -10,13 +17,30 @@ export default async function start(
     ...serverOptions,
     customServer: false,
   })
-  const srv = http.createServer(app.getRequestHandler())
-  await new Promise((resolve, reject) => {
-    // This code catches EADDRINUSE error if the port is already in use
-    srv.on('error', reject)
-    srv.on('listening', () => resolve())
-    srv.listen(port, hostname)
-  })
+
+  if (serverOptions.http2) {
+    const { key, cert } = getCerts(serverOptions.dir)
+
+    const server: Http2Server = http2.createSecureServer(
+      { key, cert, allowHTTP1: true },
+      app.getRequestHandler()
+    )
+
+    server.on('error', (error: Error) => {
+      throw error
+    })
+
+    server.listen(port, hostname)
+  } else {
+    const server: Server = http.createServer(app.getRequestHandler())
+
+    await new Promise((resolve, reject) => {
+      server.on('error', reject)
+      server.on('listening', () => resolve())
+      server.listen(port, hostname)
+    })
+  }
+
   // It's up to caller to run `app.prepare()`, so it can notify that the server
   // is listening before starting any intensive operations.
   return app
